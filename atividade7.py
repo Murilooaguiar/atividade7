ed86d31240df81c2d2e8a0a58a1507e1da01701b
Murilooaguiar <146784737+Murilooaguiar@users.noreply.github.com>
2023-10-17 11:39:20 -0300

    atividade7

---
+++ atividade7.py
@@ -0,0 +1,7 @@
+# Digite um valor e veja quantos dolares voce poderá comprar: R$
+RS = float(input("digite o valor reais"))
+dolar = float(input("digite o valor da cotação do dia"))
+conve= RS/dolar 
+print (F" seu valor em reais {RS}")
+print(F"cotação atual em dola{dolar}")
+print (f" valor da convenção {conve}")
